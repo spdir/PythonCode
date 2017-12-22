bc8944e6ed8f073593880424d978595195bf5d07
Musker.Chao <aery_mzc9123@163.com>
2017-12-22 23:39:51 +0800

    Create quick_sort.py

---
+++ Practice/quick_sort.py
@@ -0,0 +1,12 @@
+#python 2.7
+ 
+def quickSort(array):
+  if len(array) <= 2:
+    return array
+  else:
+    provi = array[0]
+    less = [i for i in array[1:] if i <= provi ]
+    gt = [i for i in array[1:] if i > provi]
+  return quickSort(less) + [provi] + quickSort(gt)
+
+print quickSort([5,3,12,66,88,99,1,2])
