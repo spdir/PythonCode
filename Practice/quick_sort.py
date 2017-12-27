dd9b4257b3ba7ecf06f97e0b8c56ab1609c7c148
Musker.Chao <aery_mzc9123@163.com>
2017-12-27 12:57:34 +0800

    Update quick_sort.py

--- Practice/quick_sort.py
+++ Practice/quick_sort.py
@@ -1,12 +1,18 @@
-#python 2.7
- 
-def quickSort(array):
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+def quickSort(array,reverse=False):
   if len(array) < 2:
     return array
   else:
     provi = array[0]
-    less = [i for i in array[1:] if i <= provi ]
-    gt = [i for i in array[1:] if i > provi]
-  return quickSort(less) + [provi] + quickSort(gt)
+    less = [i for i in array[1:] if i <= provi]
+    geater = [i for i in array[1:] if i > provi]
+  if reverse:
+    return quickSort(geater, reverse=True) + [provi] + quickSort(less,reverse=True)
+  else:
+    return quickSort(less) + [provi] + quickSort(geater)
 
-print quickSort([5,3,12,66,88,99,1,2])
+mylist = [2,55,66,22,43,67,12]
+
+print quickSort(mylist,reverse=False)
